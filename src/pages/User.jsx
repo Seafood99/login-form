021bd40f64a814f799d0d7f1a564224becf595f2
Dawam <muhammadawamali@gmail.com>
2024-08-28 19:13:41 +0700

    chore: Add token validation and redirection for unauthorized users

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function User() {
+
+  const Navigate = useNavigate()
+
+  function getTokenLocalStorage() {
+    return localStorage.getItem('token')
+  }
+
+  useEffect(() => {
+    // cek apakah token ada
+    if (!getTokenLocalStorage()) {
+      Navigate('/')
+    }
+    // cek apakah token benar
+    fetch('https://dummyjson.com/auth/me', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${getTokenLocalStorage()}`,
+      },
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.message === "Invalid/Expired Token!") {
+          localStorage.removeItem('token')
+          Navigate('/')
+        }
+      });
+  }, [])
+
+
   return (
     <div>
       halaman users
