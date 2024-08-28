169905d330d0cf3ff12aacbd985f32f46cdef0e6
Dawam <muhammadawamali@gmail.com>
2024-08-28 19:04:31 +0700

    feat: Add User page and route
    This commit adds a new page called User and updates the App component to include a route for the User page. The User page is a simple component that displays the text "halaman users". This change was made to enhance the functionality of the application by providing a new page for users to view.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // import pages
 import Home from './pages/Home'
+import User from './pages/User'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/users" element={<User />} />
       </Routes>
     </BrowserRouter>
   )
